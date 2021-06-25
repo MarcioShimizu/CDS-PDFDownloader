69130bb3869ebabd72d9f8a4190b3a73d229ea79
MarcioShimizu <marcio.shimizu@hotmail.com>
2021-06-25 20:04:41 +0900

    melhorando script

--- pdfdownloader.py
+++ pdfdownloader.py
@@ -14,37 +14,56 @@ def pdfdownloader(url, endereco):
         
         with open(endereco, 'wb') as new_file:
             new_file.write(resp.content)
-            print(f"Download finalizado com sucesso! \n")
             
 
         resp.raise_for_status()
     else:
         print('Não Encontrado...')
     
+def pdfcount(link):
+    print('Verificando quantidade de material disponível...')
+    link_num = link[-13:-12]
+    disponivel = []
+    quantidade = 0
+    if link_num == 'I' or 'II':
+        numero = ['I','II','III','IV','V','VI','VII']
+    else:
+	    numero = ['1', '2', '3', '4', '5', '6', '7']
+     
+    for i in tqdm(numero):
+        url = link[:-13] + i + link[-12:]
+        resp = requests.get(url)
+        if resp.status_code == requests.codes.OK: #pylint: disable=E1101
+            disponivel.append(i)
+            quantidade = quantidade + 1
+    print(f'Encontramos {quantidade} livros, preparando Download...')
+    return disponivel
+             
+         
+    
+
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('link1', type=str, help='Primeira parte do link')
-    parser.add_argument('numero_do_modulo', help='Numero do Módulo')
-    parser.add_argument('link2', type=str, help='Segunda parte do link')
     parser.add_argument('nome', type=str, help='nome da materia')
     args = parser.parse_args()
     print(f"-------- {args.nome} ---------")
 
     url = args.link1
-    url2 = args.link2
-    if args.numero_do_modulo == 'I' or 'II':
-        numero = ['I','II','III','IV','V','VI','VII']
-    else:
-	    numero = ['1', '2', '3', '4', '5', '6', '7']
+    numero = pdfcount(url)
 
     materia = args.nome
     os.mkdir(f'output/{materia}')
+    
     for i in tqdm(numero):
         file_name = os.path.join(f'output/{materia}', materia+f' - Unidade {i}.pdf')
-        print(f'baixando {materia} - Unidade {i}.pdf ...')
-        pdfdownloader(url+i+url2, file_name)
+        url_download = url[:-13] + i + url[-12:]
+
+        pdfdownloader(url_download, file_name)
+    print(f'Download finalizado com sucesso! Verifique na pasta output/{materia}')
             
 
 
